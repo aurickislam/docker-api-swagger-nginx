bc7f1f705a09803332ec9d3186e489ac6bf75b63
Aurick <imadul.islam@dsinnovators.com>
2024-08-14 15:42:49 +0600

    Swagger UI resource updated

--- swagger-ui/swagger-initializer.js
+++ swagger-ui/swagger-initializer.js
@@ -3,7 +3,6 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    // url: "/cm-api-2024-1.yaml",
     url: "/v1.46.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
